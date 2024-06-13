8ffae9bfa1eeb9c7f42997801e8d04a7dfb55045
lhw0841 <58808565+lhw0841@users.noreply.github.com>
2024-06-13 20:21:01 +0900

    Update main.py

--- 파일/code/main.py
+++ 파일/code/main.py
@@ -4,8 +4,8 @@ from tensorflow.keras.layers import Dropout, BatchNormalization
 from tensorflow.keras.callbacks import EarlyStopping
 
 # 데이터셋 경로
-train_dir = 'C:/Users/LEE/PycharmProjects/sw_dev/dataset/car-damage-dataset/data1a/training'
-validation_dir = 'C:/Users/LEE/PycharmProjects/sw_dev/dataset/car-damage-dataset/data1a/validation'
+train_dir = 'C:/Users/LEE/PycharmProjects/sw_dev/dataset/car-part-dataset/train'
+validation_dir = 'C:/Users/LEE/PycharmProjects/sw_dev/dataset/car-part-dataset/test'
 
 # 이미지 데이터 제너레이터 생성
 # 데이터 증강을 사용하여 모델의 일반화 성능을 향상시킵니다.
@@ -21,14 +21,14 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 # ImageDataGenerator 의 flow_from_directory 메서드 호출
 train_generator = train_datagen.flow_from_directory(
         train_dir,
-        target_size=(224, 224),
-        batch_size=20,
+        target_size=(224, 224),# resizing 224x224
+        batch_size=10,
         class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
         validation_dir,
         target_size=(224, 224),
-        batch_size=20,
+        batch_size=10,
         class_mode='binary')
 
 # ResNet50 모델 사용
@@ -49,8 +49,11 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.GlobalAveragePooling2D(),
   Dropout(0.5), # 드롭아웃을 추가하여 과적합을 방지합니다.
   BatchNormalization(), # 배치 정규화를 추가하여 학습을 안정화하고 속도를 높입니다.
+  tf.keras.layers.Dense(256, activation='relu'), # 추가된 Dense 레이어
+  tf.keras.layers.Dense(128, activation='relu'), # 추가된 Dense 레이어
   tf.keras.layers.Dense(1, activation='sigmoid')
 ])
+
 
 model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.0001),
               loss='binary_crossentropy',
@@ -60,11 +63,12 @@ model.summary()
 
 # 조기 종료 콜백 추가
 # 검증 세트에 대한 성능이 더 이상 향상되지 않을 때 학습을 중단하여 과적합을 방지합니다.
+
 early_stopping = EarlyStopping(monitor='val_loss', patience=3)
 
 history = model.fit(
       train_generator,
-      epochs=10,
+      epochs=20,
       validation_data=validation_generator,
       callbacks=[early_stopping]) # 조기 종료 콜백 사용
 
@@ -73,5 +77,10 @@ loss, accuracy = model.evaluate(validation_generator)
 print('Test accuracy :', accuracy)
 print('Test loss :', loss)
 
+model.summary()
+
 #Test accuracy : 0.676086962223053
 #Test loss : 0.8829212188720703
+
+#Test accuracy : 0.8445377945899963
+#Test loss : 0.4630492925643921
